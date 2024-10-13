c49eeab4aa68b3345d0ca6ec83aa24ed6c563e80
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-13 22:24:14 +0000

    finished project

--- app.rb
+++ app.rb
@@ -42,16 +42,20 @@ get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
   # Use HTTP.get to retrieve the API data
   @raw_response = HTTP.get(api_url)
 
   # Get the body of the response as a string
   @raw_string = @raw_response.to_s
-
+  
   # Convert the string to JSON
   @parsed_data = JSON.parse(@raw_string)
-
   
+  @currency_names = @parsed_data.fetch("currencies")
+
+  erb(:step_one)
+
   # Some more code to parse the URL and render a view template.
   # (HINT: this code is identical to the first route, you just
   # render a different view template at the end.)
@@ -63,5 +67,15 @@ get("/:from_currency/:to_currency") do
 
   api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
   
+  @raw_response = HTTP.get(api_url)
+
+  # Get the body of the response as a string
+  @raw_string = @raw_response.to_s
+  
+  # Convert the string to JSON
+  @parsed_data = JSON.parse(@raw_string)
+
+  
+  erb(:step_two)
   # Some more code to parse the URL and render a view template.
 end
